9eaf4e6fe100730acb16b7e305df614a25fec348
Vincent Tavernier <vince.tavernier@gmail.com>
2021-01-24 03:17:43 +0100

    Print failing test names and highlight differences

--- test/main.cpp
+++ test/main.cpp
@@ -10,7 +10,7 @@ template <typename Callable>
 int test_sort_function(std::vector<sort_data_t> &working_vector,
                        const std::vector<sort_data_t> &source_vector,
                        const std::vector<sort_data_t> &sorted_vector,
-                       Callable fn) {
+                       Callable fn, const char *name) {
   // Initialize working array with unsorted vector
   std::copy(source_vector.begin(), source_vector.end(), working_vector.begin());
 
@@ -20,6 +20,16 @@ int test_sort_function(std::vector<sort_data_t> &working_vector,
   // Compare sorted reference and our function
   for (size_t i = 0; i < sorted_vector.size(); ++i) {
     if (working_vector[i] != sorted_vector[i]) {
+      std::cerr << name << ": error at index " << i << std::endl;
+      for (size_t j = 0; j < sorted_vector.size(); ++j) {
+        auto &item{working_vector[j]};
+        if (item != sorted_vector[j])
+          std::cerr << "\033[0;31m" << item << "\033[0m ";
+        else
+          std::cerr << "\033[0;32m" << item << "\033[0m ";
+      }
+      std::cerr << std::endl;
+
       return 1;
     }
   }
@@ -43,15 +53,15 @@ int main() {
     std::sort(sorted_vector.begin(), sorted_vector.end());
 
     res += test_sort_function(working_vector, source_vector, sorted_vector,
-                              sort_insertion);
+                              sort_insertion, "sort_insertion");
     res += test_sort_function(working_vector, source_vector, sorted_vector,
-                              sort_selection);
+                              sort_selection, "sort_selection");
     res += test_sort_function(working_vector, source_vector, sorted_vector,
-                              sort_merge);
+                              sort_merge, "sort_merge");
     res += test_sort_function(working_vector, source_vector, sorted_vector,
-                              sort_heap);
+                              sort_heap, "sort_heap");
     res += test_sort_function(working_vector, source_vector, sorted_vector,
-                              sort_quick);
+                              sort_quick, "sort_quick");
   }
 
   return res;
